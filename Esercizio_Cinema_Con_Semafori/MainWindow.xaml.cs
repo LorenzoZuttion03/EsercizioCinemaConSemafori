7ac91919bd21aaa1e6772b984ecbbbcb631da43a
LorenzoZuttion03 <lorenzo.zuttion.stud@ispascalcomandini.it>
2021-02-12 19:20:00 +0100

    Seconda Versione

--- Esercizio_Cinema_Con_Semafori/MainWindow.xaml.cs
+++ Esercizio_Cinema_Con_Semafori/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Esercizio_Cinema_Con_Semafori
 {
@@ -20,9 +21,97 @@ namespace Esercizio_Cinema_Con_Semafori
     /// </summary>
     public partial class MainWindow : Window
     {
+        static int buffer = 5;
+        private static object x = new object();
+        private bool[] posti = new bool[10];
+        
+       
+        //private static Semaphore _pool;
         public MainWindow()
         {
-            InitializeComponent();
+            for(int i = 0; i < 10; i++)
+            {
+                posti[i] = false; // Posti tutti liberi
+            }
+            
+
+            //_pool = new Semaphore(0, 1);
+
+            
+
+            //_pool.Release(1);
+        }
+        void Cassa1()
+        {
+            int tmp = 1;
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+               tmp = int.Parse(txtCassa1.Text);
+            }));
+
+            lock (x)
+            {
+                if(posti[tmp-1] == false)
+                {
+                    posti[tmp - 1] = true;
+                }
+                else
+                {
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        MessageBox.Show("Posto occupato");
+                    }));
+                }
+
+            }
+                //se x=1 significa semaforo verde quindi posso eseguire le istruzioni, se x=0 semaforo rosso
+
+            
+                  //_pool.WaitOne();
+
+                  
+                
+
+            
+        }
+        void Cassa2()
+        {
+            
+            int tmp = 1;
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                tmp = int.Parse(txtCassa2.Text);
+            }));
+
+            lock (x)
+            {
+                if (posti[tmp - 1] == false)
+                {
+                    posti[tmp - 1] = true;
+                }
+                else
+                {
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        MessageBox.Show("Posto occupato");
+                    }));
+                }
+
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread t1 = new Thread(new ThreadStart(Cassa1));
+            t1.Start();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Thread t2 = new Thread(new ThreadStart(Cassa2));
+            t2.Start();
         }
     }
 }
